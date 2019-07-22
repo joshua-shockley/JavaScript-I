fba67b1ba9af5e0b283fed489687be47c2f5e450
Joshua-Shockley <thelumbashockley@gmail.com>
2019-07-22 15:19:25 -0500

    got the object.js done

--- assignments/objects.js
+++ assignments/objects.js
@@ -11,35 +11,79 @@
 
 // Example format of an intern object: 1,examples@you.edu,Example,F
 const example = {
-  "id": 0,
-  "name": "Example",
-  "email": "examples@you.edu",
-  "gender": "F"
+    "id": 0,
+    "name": "Example",
+    "email": "examples@you.edu",
+    "gender": "F"
 }
 
 // Write your intern objects here:
+const person1 = {
+    id: 1,
+    email: "mmelloy0@psu.edu",
+    firstName: "Mitzi",
+    gender: "F"
+};
+
+
+const person2 = {
+    id: 2,
+    email: "kdiben1@tinypic.com",
+    firstName: "Kennan",
+    gender: "M",
+    speak: function() {
+        return "Hello, my name is Kennan!";
+    }
+};
+
+
+const person3 = {
+    id: 3,
+    email: "kmummery2@wikimedia.org",
+    firstName: "Keven",
+    gender: "M"
+};
+
+
+const person4 = {
+    id: 4,
+    email: "gmartinson3@illinois.edu",
+    firstName: "Gannie",
+    gender: "M"
+};
+
+
+const person5 = {
+    id: 5,
+    email: "adaine5@samsung.com",
+    firstName: "Antonietta",
+    gender: "F",
+    multiply: function(num1, num2) {
+        return num1 * num2;
+    }
+};
 
 
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
-
+console.log(person1.firstName);
 // Kennan's ID
-
+console.log(person2.id);
 // Keven's email
-
+console.log(person3.email);
 // Gannie's name
-
+console.log(person4.firstName);
 // Antonietta's Gender
-
+console.log(person5.gender);
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
-
+console.log(person2.speak());
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
-
+console.log(person5.multiply(2, 3));
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
 // ==== Stretch Challenge: Nested Objects and the this keyword ==== 
